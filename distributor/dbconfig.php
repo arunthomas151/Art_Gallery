d3e5813d826bd31f6637ebb636c621d13716ef88
arunthomas151 <arunthomas@mca.ajce.in>
2019-11-16 11:47:11 +0530

    16/11/2019

--- distributor/dbconfig.php
+++ distributor/dbconfig.php
@@ -3,6 +3,6 @@
 	define('DB_SERVER','localhost');
 	define('DB_USER','root');
 	define('DB_PASS' ,'');
-	define('DB_NAME', 'gms');
+	define('DB_NAME', 'art_gallery');
 	$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
 ?>
