24350d253a56ae8d6ac3e5b79313073eb2316670
arunthomas151 <arunthomas@mca.ajce.in>
2019-11-27 08:33:30 +0530

    27/11/2019

---
+++ art_gallery.sql
@@ -0,0 +1,342 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 27, 2019 at 04:02 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `art_gallery`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `art`
+--
+
+CREATE TABLE `art` (
+  `id` int(11) NOT NULL,
+  `login_id` int(11) NOT NULL,
+  `artname` varchar(150) NOT NULL,
+  `imagepath` varchar(100) NOT NULL,
+  `specification` varchar(250) NOT NULL,
+  `price` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `status` varchar(15) NOT NULL DEFAULT 'Deactive'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `art`
+--
+
+INSERT INTO `art` (`id`, `login_id`, `artname`, `imagepath`, `specification`, `price`, `stock`, `status`) VALUES
+(1, 1, 'test', 'uploads/IMG_20180422_181055_HDR.jpg', 'test', 250, 4, 'Active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `id` int(11) NOT NULL,
+  `date` varchar(20) NOT NULL,
+  `buyer_id` varchar(20) NOT NULL,
+  `art_id` int(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `total` int(11) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `booking`
+--
+
+INSERT INTO `booking` (`id`, `date`, `buyer_id`, `art_id`, `name`, `mobile`, `total`, `status`) VALUES
+(1, '2019/11/26', '1', 1, 'test', '9544103818', 300, 'booked');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buyer`
+--
+
+CREATE TABLE `buyer` (
+  `id` int(10) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `mobile` varchar(30) NOT NULL,
+  `dob` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `location` varchar(30) NOT NULL,
+  `dis` varchar(30) NOT NULL,
+  `state` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `buyer`
+--
+
+INSERT INTO `buyer` (`id`, `name`, `address`, `mobile`, `dob`, `email`, `location`, `dis`, `state`, `password`) VALUES
+(1, 'test', 'test', '9544103818', '1995-12-01', 'test@gmail.com', 'Thoprankudy', 'Idukki', 'Kerala', '9544103818');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `confirm`
+--
+
+CREATE TABLE `confirm` (
+  `id` int(10) NOT NULL,
+  `cname` varchar(30) NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `nos` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `id` int(11) NOT NULL,
+  `state` varchar(30) NOT NULL,
+  `dis` varchar(30) NOT NULL,
+  `loc` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `location`
+--
+
+INSERT INTO `location` (`id`, `state`, `dis`, `loc`) VALUES
+(1, 'Kerala', 'Idukki', 'Thoprankudy');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(10) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(9) NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `link` varchar(30) NOT NULL,
+  `orderid` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id`, `title`, `link`, `orderid`) VALUES
+(4, 'Logout', 'logout.php', 2),
+(6, 'View Orders', 'view_orders.php', 3),
+(7, 'Cancelled Orderd', 'cancelled.php', 5),
+(8, 'Add Product', 'add_product.php', 6),
+(9, 'Change passwod', 'change_password.php', 7),
+(10, 'View Products', 'view_products.php', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+CREATE TABLE `notification` (
+  `id` int(10) NOT NULL,
+  `login_id` varchar(12) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `date` varchar(20) NOT NULL,
+  `msg` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `notification`
+--
+
+INSERT INTO `notification` (`id`, `login_id`, `book_id`, `date`, `msg`) VALUES
+(1, '1', 1, '2019/11/26', 'Your booking is orderd.Expecte deliver on 2019-12-06');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seller`
+--
+
+CREATE TABLE `seller` (
+  `id` int(11) NOT NULL,
+  `cname` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `mobile` bigint(14) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `locality` varchar(30) NOT NULL,
+  `dis` varchar(30) NOT NULL,
+  `state` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `seller`
+--
+
+INSERT INTO `seller` (`id`, `cname`, `name`, `address`, `mobile`, `email`, `locality`, `dis`, `state`, `password`) VALUES
+(1, 'Test', 'Test', 'Test', 9544103818, 'Test@gmail.com', 'Thoprankudy', 'Idukki', 'Kerala', '9544103818');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `art`
+--
+ALTER TABLE `art`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `buyer`
+--
+ALTER TABLE `buyer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `confirm`
+--
+ALTER TABLE `confirm`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `seller`
+--
+ALTER TABLE `seller`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `art`
+--
+ALTER TABLE `art`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `buyer`
+--
+ALTER TABLE `buyer`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `confirm`
+--
+ALTER TABLE `confirm`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `notification`
+--
+ALTER TABLE `notification`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `seller`
+--
+ALTER TABLE `seller`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
