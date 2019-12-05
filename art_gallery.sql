05345c26e82079d1b71083b988427fbbefd1e883
arunthomas151 <arunthomas@mca.ajce.in>
2019-12-05 22:39:52 +0530

    05/12/2019

--- art_gallery.sql
+++ art_gallery.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 29, 2019 at 03:53 PM
+-- Generation Time: Dec 05, 2019 at 06:03 PM
 -- Server version: 10.4.8-MariaDB
 -- PHP Version: 7.3.10
 
@@ -39,6 +39,154 @@ CREATE TABLE `art` (
   `status` varchar(15) NOT NULL DEFAULT 'Deactive'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `id` int(11) NOT NULL,
+  `date` varchar(20) NOT NULL,
+  `buyer_id` varchar(20) NOT NULL,
+  `art_id` int(20) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `total` int(11) NOT NULL,
+  `status` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buyer`
+--
+
+CREATE TABLE `buyer` (
+  `id` int(10) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `mobile` varchar(30) NOT NULL,
+  `dob` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `location` varchar(30) NOT NULL,
+  `dis` varchar(30) NOT NULL,
+  `state` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `status` varchar(15) NOT NULL DEFAULT 'Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `confirm`
+--
+
+CREATE TABLE `confirm` (
+  `id` int(10) NOT NULL,
+  `cname` varchar(30) NOT NULL,
+  `type` varchar(30) NOT NULL,
+  `nos` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `id` int(11) NOT NULL,
+  `state` varchar(30) NOT NULL,
+  `dis` varchar(30) NOT NULL,
+  `loc` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `location`
+--
+
+INSERT INTO `location` (`id`, `state`, `dis`, `loc`) VALUES
+(1, 'Kerala', 'Kottayam', 'Kanjirapally');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(10) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(9) NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `link` varchar(30) NOT NULL,
+  `orderid` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `menu`
+--
+
+INSERT INTO `menu` (`id`, `title`, `link`, `orderid`) VALUES
+(4, 'Logout', 'logout.php', 2),
+(6, 'View Orders', 'view_orders.php', 3),
+(7, 'Cancelled Orderd', 'cancelled.php', 5),
+(8, 'Add Product', 'add_product.php', 6),
+(9, 'Change passwod', 'change_password.php', 7),
+(10, 'View Products', 'view_products.php', 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notification`
+--
+
+CREATE TABLE `notification` (
+  `id` int(10) NOT NULL,
+  `login_id` varchar(12) NOT NULL,
+  `book_id` int(11) NOT NULL,
+  `date` varchar(20) NOT NULL,
+  `msg` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seller`
+--
+
+CREATE TABLE `seller` (
+  `id` int(11) NOT NULL,
+  `cname` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `address` varchar(50) NOT NULL,
+  `mobile` bigint(14) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `locality` varchar(30) NOT NULL,
+  `dis` varchar(30) NOT NULL,
+  `state` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `status` varchar(15) NOT NULL DEFAULT 'Active'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Indexes for dumped tables
 --
@@ -50,6 +198,54 @@ ALTER TABLE `art`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `buyer`
+--
+ALTER TABLE `buyer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `confirm`
+--
+ALTER TABLE `confirm`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `notification`
+--
+ALTER TABLE `notification`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `seller`
+--
+ALTER TABLE `seller`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -57,6 +253,54 @@ ALTER TABLE `art`
 -- AUTO_INCREMENT for table `art`
 --
 ALTER TABLE `art`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `buyer`
+--
+ALTER TABLE `buyer`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `confirm`
+--
+ALTER TABLE `confirm`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `notification`
+--
+ALTER TABLE `notification`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `seller`
+--
+ALTER TABLE `seller`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
